b31f5f2fb4a414200d46b090b831de8e99c6fbfd
Kevin O'Keeffe <kevokeeffe@gmail.com>
2020-02-06 00:17:42 +0000

    Home page update - google fonts and button config

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,17 +6,19 @@
           <b-tab no-body title="Welcome" active>
             <b-card-img
               bottom
+              class="b-card-image-welcome"
               src="https://sociology.uconn.edu/wp-content/uploads/sites/2172/2019/06/WELCOME-ST-IVES.jpg"
             ></b-card-img>
+            <h2 class="h2-welcome">to HARMON€Y</h2>
           </b-tab>
-          <b-tab title="Joining"><p>I'm the first tab</p></b-tab>
+          <b-tab title="Why Join?"><p>Because we love you</p></b-tab>
           <b-tab title="About">
             <b-card-img
               bottom
-              class="b-card-image-home"
+              class="b-card-image-eco"
               src="https://s3-us-west-2.amazonaws.com/medici-prod/root/Medici/public/uploads/custom2_fintechs-for-sustainable-finance-trends-and-opportunities.png?1574922914"
             ></b-card-img>
-            <p class="text-about-home">
+            <p class="text-about-eco">
               Being environmentally conscious can mean something different to
               each person. For us, eco-friendly decision-making drives our
               behavior, minimizing our environmental impact and harm.
@@ -25,11 +27,11 @@
         </b-tabs>
       </div>
       <div class="mt-3">
-        <b-button-group>
-          <b-button variant="success" to="/login">Login</b-button>
-          <b-button variant="info" to="/about">Info</b-button>
-          <b-button variant="warning" to="/register">Sign-Up</b-button>
-        </b-button-group>
+          <b-button class='b-button-in-card-login' variant="success" to="/login">Login</b-button>
+          <b-button class='b-button-in-card-sign-up' variant="warning" to="/register">Sign-Up</b-button>
+      </div>
+      <div>
+        <b-button class='b-button-in-card-info' variant="info" to="/about">Info</b-button>
       </div>
     </b-card>
   </div>
@@ -71,15 +73,41 @@ export default {
   );
 }
 
-.b-card-image-home {
+.b-card-image-eco {
   width: 50%;
   float: left;
-  margin-right: 50%;
-  margin-bottom: 5%;
+  margin-right: 100%;
+  margin-bottom: 20px;
+  max-width: 200px;
 }
 
-.text-about-home {
+.b-button-in-card-login{
+  width: 80px;
+  margin: 4px;
+  padding: 10px;
+  position: relative;
+}
+.b-button-in-card-info{
+  width: 80px;
+  padding: 6px;
+  margin:10px;
+  position: relative;
+}
+.b-button-in-card-sign-up{
+  width: 80px;
+   margin: 4px;
+   padding: 10px;
+  position: relative;
+}
+.b-card-image-welcome{
+  max-width: 300px;
+  position: relative;
+}
+
+.text-about-eco {
   text-align: left;
+  max-width: 400px;
+  position: relative;
 }
 
 .b-card-home {
@@ -90,13 +118,16 @@ export default {
   width: 80%;
   margin-top: 60px;
   height: 70%;
+  max-width: 600px;
 }
 
 .h1-home {
   position: relative;
   font-family: 'Open Sans', sans-serif;
 }
-
+.h2-welcome{
+  font-family: 'Varela Round', sans-serif;
+}
 .b-button-home {
 }
 </style>

6f92929b535e396145623cc99a0cfa8bc0a71765
Kevin O'Keeffe <kevokeeffe@gmail.com>
2020-04-10 23:44:09 +0100

    UX overhaul for mobile

--- src/views/pages/Notifications.vue
+++ src/views/pages/Notifications.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="notifications">
     <h1>This is the Notification Page</h1>
+    
   </div>
 </template>
--- src/views/pages/UserProfile.vue
+++ src/views/pages/UserProfile.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="user-profile">
     <h1>This is the User Profile</h1>
+    <b-jombotron class="delete-account">
+      
+    </b-jombotron>
   </div>
 </template>
